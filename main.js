2a49077a81a4ccafd7440c6fdb8c1203ff82871a
Antonio Pereira <antonio_m_sp@hotmail.com>
2017-10-08 01:27:52 +0100

    asd

--- main.js
+++ main.js
@@ -31,7 +31,8 @@ main=function()
 	// get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml 
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor) 
 	
-	var filename=getUrlVars()['file'] || "ourScene.xml";
+	//var filename=getUrlVars()['file'] || "testXML.xml";
+    var filename=getUrlVars()['file'] || "ourScene.xml";
 
 	// create and load graph, and associate it to scene. 
 	// Check console for loading errors
