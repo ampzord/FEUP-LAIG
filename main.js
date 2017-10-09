45a635698d932f3f3c0b0b6738454aa1c002d2e1
Huguinho <huguinho27@users.noreply.github.com>
2017-10-09 10:25:00 +0100

    ertghgh

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ main=function()
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor) 
 	
 	//var filename=getUrlVars()['file'] || "testXML.xml";
-    var filename=getUrlVars()['file'] || "ourScene.xml";
+    var filename=getUrlVars()['file'] || "room.xml";
 
 	// create and load graph, and associate it to scene. 
 	// Check console for loading errors
