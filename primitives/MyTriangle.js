9b7aaa9ef59d8e0127da9c439aecac9746e9a44a
Huguinho <huguinho27@users.noreply.github.com>
2017-10-20 12:22:21 +0100

    triangles done yeah!

--- primitives/MyTriangle.js
+++ primitives/MyTriangle.js
@@ -1,75 +1,58 @@
+function MyTriangle(scene, args) {
+    CGFobject.call(this,scene);
 
-/**
- * MyTriangle
- * @constructor
- * @args coordinates of each vertex(x,y,z)
- */
- function MyTriangle(scene, args) {
- 	CGFobject.call(this,scene);
-  
-  	this.args = [];
-	args = args.split(" ");
-	for(let i = 0; i < args.length; i++){
-			this.args.push(parseFloat(args[i]));
-	}
+    this.args = args.split(" ");
+    for(var i = 0; i < this.args.length; i++){
+        this.args[i] = parseFloat(this.args[i]);
+    }
 
-  this.initBuffers(args);
- };
+    this.args = args;
+    this.initBuffers();
+};
 
 MyTriangle.prototype = Object.create(CGFobject.prototype);
 MyTriangle.prototype.constructor=MyTriangle;
 
- MyTriangle.prototype.initBuffers = function(args) {
+MyTriangle.prototype.initBuffers = function () {
+
+    var points = this.args.split(" ");
+    this.a = Math.sqrt(Math.pow(points[0] - points[6], 2) + Math.pow(points[1] - points[7], 2) + Math.pow(points[2] - points[8], 2));
+    this.b = Math.sqrt(Math.pow(points[3] - points[0], 2) + Math.pow(points[4] - points[1], 2) + Math.pow(points[5] - points[2], 2));
+    this.c = Math.sqrt(Math.pow(points[6] - points[3], 2) + Math.pow(points[7] - points[4], 2) + Math.pow(points[8] - points[5], 2));
+    
+    this.cos_a = Math.cos((- Math.pow(this.a, 2) + Math.pow(this.b, 2) + Math.pow(this.c, 2))/(2 * this.b * this.c));
+    this.cos_b = Math.cos((Math.pow(this.a, 2) - Math.pow(this.b, 2) + Math.pow(this.c, 2))/(2 * this.b * this.c));
+    this.cos_c = Math.cos((Math.pow(this.a, 2) + Math.pow(this.b, 2) - Math.pow(this.c, 2))/(2 * this.b * this.c));
+    this.sin_b = Math.sqrt(1 - Math.pow(this.cos_b, 2));
 
     this.vertices = new Array();
     this.indices = new Array();
     this.normals = new Array();
-    this.baseTexCoords = new Array();
+    this.texCoords = new Array();
 
-    this.vertices.push(this.args[0],this.args[1],this.args[2]);
-    this.vertices.push(this.args[3],this.args[4],this.args[5]);
-    this.vertices.push(this.args[6],this.args[7],this.args[8]);
+    this.vertices.push(points[0],points[1],points[2]);
+    this.vertices.push(points[3],points[4],points[5]);
+    this.vertices.push(points[6],points[7],points[8]);
 
-    this.indices.push(0, 1, 2);
+    this.indices.push(0,1,2);
 
-   	var vec1 = [
-        this.args[0]-this.args[3],
-        this.args[1]-this.args[4],
-        this.args[2]-this.args[5]
-    ];
+    this.normals.push(0,1,0);
+    this.normals.push(0,1,0);
+    this.normals.push(0,1,0);
 
-    var vec2 = [
-        this.args[0]-this.args[6],
-        this.args[1]-this.args[7],
-        this.args[2]-this.args[8]
-    ];
-
-    var normal = vec3.create();
-
-    vec3.cross(normal,vec1,vec2);
-
-    this.normals = [
-        vec3[0],vec3[1],vec3[2],
-        vec3[0],vec3[1],vec3[2],
-        vec3[0],vec3[1],vec3[2]
-    ];
-
-    this.baseTexCoords.push(0,0);
-    this.baseTexCoords.push(1,0);
-    this.baseTexCoords.push(0,1);
-
-    
-	this.texCoords = new Array(this.baseTexCoords.length);
+    this.texCoords.push(0,0);
+    this.texCoords.push(0,1);
+    this.texCoords.push(1,1);
 
     this.primitiveType=this.scene.gl.TRIANGLES;
     this.initGLBuffers();
 };
 
-MyTriangle.prototype.applyAf = function (afS,afT)
-{
-    for (var i = 0; i < this.baseTexCoords.length; i+=2) {
-        this.texCoords[i] = this.baseTexCoords[i]/afS;
-        this.texCoords[i+1] = this.baseTexCoords[i+1]/afT;
-    }
+MyTriangle.prototype.applyAf = function(ampS, ampT){
+    this.texCoords = [
+            0, 1,
+            this.c/ampS, 1,
+            (this.c - this.a * this.cos_b)/ampS, (1 - this.a * this.sin_b)/(ampT)
+        ];
     this.updateTexCoordsGLBuffers();
 };
